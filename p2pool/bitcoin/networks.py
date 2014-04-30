dec3305fb9b436cea5300cee57f3ff72dd66519e
m0gliE <m0gliEz@gmail.com>
2014-04-29 21:27:16 -0700

    Update for zimstake

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -208,6 +208,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    zimstake=math.Object(
+        P2P_PREFIX='69420522'.decode('hex'),
+        P2P_PORT=12125,
+        ADDRESS_VERSION=32,
+        RPC_PORT=12126,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'zimstakecoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 512*100000000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=60, # s
+        SYMBOL='ZS',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Zimstake') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Zimstake/') if platform.system() == 'Darwin' else os.path.expanduser('~/.zimstake'), 'zimstake.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explore.zimstake.us/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explore.zimstake.us/address/',
+        TX_EXPLORER_URL_PREFIX='http://explore.zimstake.us/tx/',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.1e8,
+    ),
 
 )
 for net_name, net in nets.iteritems():
