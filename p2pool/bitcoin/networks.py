99fffdfb59a92afe80a6074401b7728f68c27572
m0gliE <m0gliEz@gmail.com>
2014-04-30 17:17:29 -0700

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -222,8 +222,8 @@ nets = dict(
         BLOCK_PERIOD=60, # s
         SYMBOL='ZS',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Zimstake') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Zimstake/') if platform.system() == 'Darwin' else os.path.expanduser('~/.zimstake'), 'zimstake.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://explore.zimstake.us/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://explore.zimstake.us/address/',
+        BLOCK_EXPLORER_URL_PREFIX='http://explore.zimstake.us/index.php?block_height=',
+        ADDRESS_EXPLORER_URL_PREFIX='http://coinia.net/zimstake/getbalance.php?addr=',
         TX_EXPLORER_URL_PREFIX='http://explore.zimstake.us/index.php?transaction=',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
         DUMB_SCRYPT_DIFF=1,
